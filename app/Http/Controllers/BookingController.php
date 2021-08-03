5e52d093fe7ae9bbb1334459025d92bd3f2cf419
Pranab Kalita <pavankalita@gmail.com>
2021-08-03 10:19:49 +0530

    Implemented the ability to get the employee availability

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Bookings\Filters\AppointmentFilter;
-use App\Bookings\Filters\SlotsPassedTodayFilter;
-use App\Bookings\Filters\UnavailabilityFilter;
-use App\Bookings\TimeSlotGenerator;
 use App\Models\Appointment;
+use App\Models\Employee;
 use App\Models\Schedule;
 use App\Models\Service;
-use Carbon\CarbonInterval;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
@@ -19,15 +15,8 @@ class BookingController extends Controller
         $schedule = Schedule::find(1);
         $service = Service::find(1);
 
-        $appointments = Appointment::whereDate('date', '2021-08-03')->get();
-
-        $slots = (new TimeSlotGenerator($schedule, $service))
-            ->applyFilters([
-                new SlotsPassedTodayFilter(),
-                new UnavailabilityFilter($schedule->unavailabilities),
-                new AppointmentFilter($appointments)
-            ])
-            ->get();
+        $employee = Employee::find(1);
+        $slots = $employee->availableTimeSlots($schedule, $service);
 
 
         return view('bookings.create', compact('slots'));
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,6 +2,11 @@
 
 namespace App\Models;
 
+use App\Bookings\Filters\AppointmentFilter;
+use App\Bookings\Filters\SlotsPassedTodayFilter;
+use App\Bookings\Filters\UnavailabilityFilter;
+use App\Bookings\TimeSlotGenerator;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,5 +22,26 @@ class Employee extends Model
     public function schedules()
     {
         return $this->hasMany(Schedule::class);
+    }
+
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class);
+    }
+
+    public function availableTimeSlots(Schedule $schedule, Service $service)
+    {
+        return (new TimeSlotGenerator($schedule, $service))
+            ->applyFilters([
+                new SlotsPassedTodayFilter(),
+                new UnavailabilityFilter($schedule->unavailabilities),
+                new AppointmentFilter($this->appointmentsForDate($schedule->date))
+            ])
+            ->get();
+    }
+
+    public function appointmentsForDate(Carbon $date)
+    {
+        return $this->appointments()->whereDate('date', $date)->get();
     }
 }
